7e91788601418484403f05821efb71443dca8a56
Abid Khan <llawlietbagsum@gmail.com>
2025-07-27 22:55:17 +0530

    shift from supabase to neon

--- app/api/waitlist/route.ts
+++ app/api/waitlist/route.ts
@@ -1,17 +1,13 @@
-import { supabase } from '../../../lib/supabaseClient';
+import sql from '@/lib/db';
 import { type NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
-  const { data, error } = await supabase
-    .from('waitlist')
-    .select('*')
-    .order('created_at', { ascending: true });
-
-  if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+  try {
+    const waitlist = await sql`SELECT * FROM waitlist ORDER BY created_at ASC`;
+    return NextResponse.json(waitlist);
+  } catch (error) {
+    return NextResponse.json({ error: (error as Error).message }, { status: 500 });
   }
-
-  return NextResponse.json(data);
 }
 
 export async function POST(request: NextRequest) {
@@ -21,14 +17,10 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ error: 'Email is required' }, { status: 400 });
   }
 
-  const { data, error } = await supabase
-    .from('waitlist')
-    .insert([{ email }])
-    .select();
-
-  if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+  try {
+    const [newUser] = await sql`INSERT INTO waitlist (email) VALUES (${email}) RETURNING *`;
+    return NextResponse.json(newUser);
+  } catch (error) {
+    return NextResponse.json({ error: (error as Error).message }, { status: 500 });
   }
-
-  return NextResponse.json(data);
 }
