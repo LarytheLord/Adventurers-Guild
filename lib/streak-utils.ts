ecd8919f8cdc4471edb38e7a530dbbe56c185878
Sakshi M <amisadha0519@gmail.com>
2026-04-17 10:05:01 +0530

    fix: changed `import prisma` to `import { prisma }`

--- lib/streak-utils.ts
+++ lib/streak-utils.ts
@@ -1,4 +1,4 @@
-import prisma from "@/lib/db";
+import { prisma } from "@/lib/db";
 
 export async function updateStreak(userId: string): Promise<void> {
   const profile = await prisma.adventurerProfile.findUnique({
